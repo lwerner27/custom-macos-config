c0d5bbd86922cebadf5a59b84efdbcdb4838aac5
Lucas Werner <lwerner27@gmail.com>
2023-06-05 23:14:00 -0500

    Initial Commit.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className='text-9xl'>Hello World</h1>
+    </div>
+  )
+
+}
+
+export default App
